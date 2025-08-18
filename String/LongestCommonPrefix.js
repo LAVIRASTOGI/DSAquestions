77bab7def3a9af0884c57bab23dd8694a6657054
LaviRastogi <1993lavirastogi@gmail.com>
2025-08-18 14:08:38 +0530

    leetcode ques

---
+++ String/LongestCommonPrefix.js
@@ -0,0 +1,29 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// time complexity: O(n^2)
+// space complexity: O(1)
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, prefix.length - 1);
+      if (prefix.length === 0) return "";
+    }
+  }
+  return prefix;
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "floght"]));
