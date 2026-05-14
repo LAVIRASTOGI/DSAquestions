736f93025a9243d52bf9881db4070547fb2f1c9d
LaviRastogi <1993lavirastogi@gmail.com>
2026-05-14 14:21:51 +0530

    BackTracking

---
+++ Backtracking/subsets78.js
@@ -0,0 +1,33 @@
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [[],[0]]
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  let result = [];
+  // path - conatins ans and start contains the next pointer
+  let backtrack = (path, start) => {
+    result.push([...path]);
+    for (let i = 0; i < nums.length; i++) {
+      path.push(nums[i]);
+      backtrack(path, i + 1);
+      path.pop();
+    }
+  };
+
+  backtrack([], 0);
+
+  return result;
+};
